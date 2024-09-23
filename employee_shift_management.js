1258167b795efeb95538a02e4813109d707c6dd0
Cameron Smith <Cs11@usf.edu>
2024-09-22 22:58:00 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Employee Array of Employee Objects
+
+const employees = [
+    {name: 'Cameron' , shifts: [{ day: 'Monday' , hours: 7} , { day: 'Tuesday' , hours: 6}] },
+    {name: 'Justin' , shifts: [{ day: 'Wednesday' , hours; 5} , { day: 'Thursday' , hours: 8}] },
+    {name: 'Madison' , shifts: [{ day: 'Friday'}] },
+    {name: 'Ava' , shifts: [{ day: 'Thursday'}] }
+
+];
